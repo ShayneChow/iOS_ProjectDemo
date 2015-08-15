decdc53b3de6668ec3ee2d5efe134869054594c1
Shayne Chow <shayne@weplus.cn>
2015-08-15 19:55:07 +0800

    项目初始化

---
+++ 百思不得姐/百思不得姐/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  百思不得姐
+//
+//  Created by Xiang on 15/8/15.
+//  Copyright (c) 2015年 周想. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
