7cd72dca38e00e42338e5d7755d4e512b2c440a9
Shayne Chow <shayne@weplus.cn>
2015-08-23 20:07:51 +0800

    Me界面搭建基本TableView

--- 百思不得姐/百思不得姐/Classes/Me-我/Controller/ZXMeViewController.m
+++ 百思不得姐/百思不得姐/Classes/Me-我/Controller/ZXMeViewController.m
@@ -8,6 +8,7 @@
 
 #import "ZXMeViewController.h"
 #import "ZXSettingViewController.h"
+#import "ZXMeCell.h"
 
 @interface ZXMeViewController ()
 
@@ -15,11 +16,18 @@
 
 @implementation ZXMeViewController
 
+static NSString * const ZXMeId = @"me";
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    self.view.backgroundColor = ZXGlobalBg;
+    [self setupNavigtion];
     
+    [self setupTableView];
+    
+}
+
+- (void)setupNavigtion {
     // 设置导航栏右边的按钮
     UIBarButtonItem *settingItem = [UIBarButtonItem itemWithTarget:self action:@selector(settingClick) image:@"mine-setting-icon" highImage:@"mine-setting-icon-click"];
     UIBarButtonItem *moonItem = [UIBarButtonItem itemWithTarget:self action:@selector(moonClick) image:@"mine-moon-icon" highImage:@"mine-moon-icon-click"];
@@ -33,6 +41,33 @@
 
 - (void)moonClick {
     ZXLogFunc;
+}
+
+- (void)setupTableView {
+    self.tableView.backgroundColor = ZXGlobalBg;
+    self.tableView.separatorStyle = UITableViewCellSeparatorStyleNone;
+    [self.tableView registerClass:[ZXMeCell class] forCellReuseIdentifier:ZXMeId];
+}
+
+#pragma mark - UITableViewControllerDataSource
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    return 2;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return 1;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    ZXMeCell *cell = [tableView dequeueReusableCellWithIdentifier:ZXMeId];
+    
+    if (indexPath.section == 0) {
+        cell.textLabel.text = @"登录/注册";
+        cell.imageView.image = [UIImage imageNamed:@"mine_icon_nearby"];
+    } else {
+        cell.textLabel.text = @"离线下载";
+    }
+    return cell;
 }
 
 @end
--- 百思不得姐/百思不得姐/Classes/Me-我/View/ZXMeCell.h
+++ 百思不得姐/百思不得姐/Classes/Me-我/View/ZXMeCell.h
@@ -1,13 +1,13 @@
 //
-//  ZXMeViewController.h
+//  ZXMeCell.h
 //  百思不得姐
 //
-//  Created by Xiang on 15/8/16.
+//  Created by Xiang on 15/8/23.
 //  Copyright (c) 2015年 周想. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
 
-@interface ZXMeViewController : UITableViewController
+@interface ZXMeCell : UITableViewCell
 
 @end
